b64ba82e8ce15a821dbda70e0e18589987c9d565
peterjohnson1000 <peterj1298@gmail.com>
2022-07-24 14:51:36 +0530

    🌎 share page -added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,19 @@ import {
   BsFillMoonFill,
   BsSunFill,
   BsFillLightningChargeFill,
+  BsFillShareFill,
+  BsCheckCircleFill,
 } from "react-icons/bs";
 import "../App.css";
 import DarkMode from "../hooks/DarkMode";
+import { useState } from "react";
 
 const Header = () => {
   const [setTheme, colorTheme] = DarkMode();
+  const [isCopied, setIsCopied] = useState(false);
 
   return (
-    <div className="container flex items-center py-4 justify-between text-xl sm:text-lg px-10 dark:bg-dPrimary dark:text-primaryC">
+    <div className="container flex items-center py-4 justify-between text-xl sm:text-lg px-5 dark:bg-dPrimary dark:text-primaryC">
       <div>
         <Link to="/">
           <BsFillLightningChargeFill />
@@ -27,7 +31,27 @@ const Header = () => {
         >
           About
         </Link>
-        <div className="pl-20 sm:pl-8" onClick={() => setTheme(colorTheme)}>
+        <div
+          className="ml-14 cursor-pointer sm:ml-10"
+          onClick={() => {
+            navigator.clipboard.writeText("https://peterjohnson.netlify.app/");
+            setIsCopied(true);
+            setTimeout(() => {
+              setIsCopied(false);
+            }, 5000);
+          }}
+        >
+          {/* {isCopied === true ? <BsCheckCircleFill /> : <BsFillShareFill />} */}
+          {isCopied === true ? (
+            <div className="font-light text-lg">Copied!</div>
+          ) : (
+            <BsFillShareFill />
+          )}
+        </div>
+        <div
+          className="ml-10 cursor-pointer"
+          onClick={() => setTheme(colorTheme)}
+        >
           {colorTheme === "light" ? <BsSunFill /> : <BsFillMoonFill />}
         </div>
       </div>
